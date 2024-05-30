dd1fe706fca617020d0138fe76fe078c919be152
Mubarak-o <mubarakomballi29@gmail.com>
2024-05-30 09:43:37 +0100

    Added React Query Devtools package

--- src/App.tsx
+++ src/App.tsx
@@ -13,43 +13,55 @@ import { LogIn } from "./pages/login";
 import { ProtectedRoute } from "./components/protectedRoute";
 import { Account } from "./pages/account";
 import { Toaster } from "react-hot-toast";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./api/api";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
 	return (
 		<AuthContextProvider>
-			<BrowserRouter>
-				<Toaster
-					position="top-center"
-					toastOptions={{
-						success: {
-							duration: 2500,
-						},
-					}}
-				/>
-				<Navbar />
-				{/* <Searchbar /> */}
-				<Routes>
-					<Route path={"/"} element={<Home />} />
-					<Route path={"/signup"} element={<SignUp />} />
-					<Route path={"/login"} element={<LogIn />} />
-					<Route
-						path="/account"
-						element={
-							<ProtectedRoute>
-								<Account />
-							</ProtectedRoute>
-						}
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter>
+					<Toaster
+						position="top-center"
+						toastOptions={{
+							success: {
+								duration: 2500,
+							},
+						}}
 					/>
-					<Route path={"/movies"}>
-						<Route index element={<Movies />} />
-						<Route path={":movieId"} element={<MovieDetails />} />
-					</Route>
-					<Route path={"tv-shows"}>
-						<Route index element={<TVShows />} />
-						<Route path={":tvShowId"} element={<TVShowDetails />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+					<Navbar />
+					{/* <Searchbar /> */}
+					<Routes>
+						<Route path={"/"} element={<Home />} />
+						<Route path={"/signup"} element={<SignUp />} />
+						<Route path={"/login"} element={<LogIn />} />
+						<Route
+							path="/account"
+							element={
+								<ProtectedRoute>
+									<Account />
+								</ProtectedRoute>
+							}
+						/>
+						<Route path={"/movies"}>
+							<Route index element={<Movies />} />
+							<Route
+								path={":movieId"}
+								element={<MovieDetails />}
+							/>
+						</Route>
+						<Route path={"tv-shows"}>
+							<Route index element={<TVShows />} />
+							<Route
+								path={":tvShowId"}
+								element={<TVShowDetails />}
+							/>
+						</Route>
+					</Routes>
+				</BrowserRouter>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</AuthContextProvider>
 	);
 }
