d4155a9e09d120ce38d6193ecba0350743b760a5
Mubarak-o <mubarakomballi29@gmail.com>
2024-07-03 12:01:30 +0100

    Refactored Media component to include filtering of initial fetched data

--- src/components/mediaList/media.tsx
+++ src/components/mediaList/media.tsx
@@ -1,14 +1,15 @@
-import { useRef, useState } from "react";
-import { FilterButtons } from "../filter/filterButtons";
+import { useMemo, useState } from "react";
 import { MediaCards } from "./mediaCards";
 import { Pagination } from "./pagination";
-import { useFilteredMedia, useSearchedMedia } from "../../api/hooks";
+import { useMediaData } from "../../api/hooks";
+import { DataFilters, MediaData } from "../../types/types";
+import { isMovie } from "../../utils/utility";
+import { Filters } from "../filter/filters";
 
 interface MediaProps {
-	fetchUrl: string;
 	mediaType: "movie" | "tv";
 }
-export const Media = ({ fetchUrl, mediaType }: MediaProps) => {
+export const Media = ({ mediaType }: MediaProps) => {
 	// const [mediaData, setMediaData] = useState<MediaData[]>([]);
 	const [currentPage, setCurrentPage] = useState<number>(1);
 	// Having cardsPerPage be a state variable might not be needed
@@ -16,79 +17,82 @@ export const Media = ({ fetchUrl, mediaType }: MediaProps) => {
 	//   for it depending on what screen media is being used, phone, tablet or PC
 	// Otherwise I think just having it as a const variable should be fine
 	const [cardsPerPage, _] = useState<number>(10);
-	const [selectedGenre, setSelectedGenre] = useState<string | undefined>(); // comma seperated string e.g: '1,25,3'
-	const [selectedSorting, setSelectedSorting] =
-		useState<string>("popularity.desc");
-	const [selectedYear, setselectedYear] = useState<string[] | undefined>();
-	const inputValueRef = useRef<HTMLInputElement>(null);
-	const [searchQuery, setSearchQuery] = useState<string>("");
+	const [filters, setFilters] = useState<DataFilters>({
+		searchQuery: "",
+		selectedGenre: undefined,
+		selectedYear: undefined,
+		selectedSorting: undefined,
+	});
 
-	const { data: filteredMediaData = [] } = useFilteredMedia(
-		fetchUrl,
-		selectedGenre,
-		selectedSorting,
-		selectedYear
-	);
+	const { data: mediaData = [], isLoading, error } = useMediaData(mediaType);
 
-	const { data: searchMediaData = [] } = useSearchedMedia(
-		searchQuery,
-		mediaType
-	);
+	const filteredData = useMemo(() => {
+		return mediaData
+			.filter((media) => {
+				if (filters.searchQuery) {
+					const title = isMovie(media) ? media.title : media.name;
+					return title
+						.toLowerCase()
+						.includes(filters.searchQuery.toLowerCase());
+				}
+				return true; // if no search query is present include all items
+			})
+			.filter((media) => {
+				if (filters.selectedGenre && filters.selectedGenre.length > 0) {
+					filters.selectedGenre.every((genre) =>
+						media.genre_ids.includes(genre)
+					);
+				}
+				return true; // if no genre is selected include all items
+			})
+			.filter((media) => {
+				if (filters.selectedYear?.length == 2) {
+					const date = isMovie(media)
+						? media.release_date
+						: media.first_air_date;
+					const releaseDate = new Date(date);
+					const startDate = new Date(filters.selectedYear[0]);
+					const endDate = new Date(filters.selectedYear[1]);
+					return releaseDate >= startDate && releaseDate <= endDate;
+				}
+				return true; // if no year is selected, include all items
+			})
+			.sort((a, b) => {
+				const getTitle = (media: MediaData) =>
+					isMovie(media) ? media.title : media.name;
 
-	const mediaData = searchQuery ? searchMediaData : filteredMediaData;
+				switch (filters.selectedSorting) {
+					case "popularity.desc": {
+						return b.popularity - a.popularity;
+					}
+					case "title.asc": {
+						return getTitle(a).localeCompare(getTitle(b));
+					}
+					default:
+						return 0; // maintain initial (default) sorting of Trending
+				}
+			});
+	}, [mediaData, filters]);
 
-	const handleSelectGenres = (selectedGenres: string) => {
-		setSelectedGenre(selectedGenres);
-	};
-
-	const handleSelectSorting = (selectedOption: string) => {
-		setSelectedSorting(selectedOption);
-	};
-
-	const handleSelectedYears = (selectedYears: string) => {
-		if (selectedYears.length < 5) {
-			const yearGte = `${selectedYears}-01-01`;
-			const yearLte = `${selectedYears}-12-31`;
-			setselectedYear([yearGte, yearLte]);
-		} else {
-			const years = selectedYears.split("-");
-			const yearGte = `${years[0]}-01-01`;
-			const yearLte = `${years[1]}-12-31`;
-			setselectedYear([yearGte, yearLte]);
-		}
-	};
-
-	function handleDisplayMediaData(): void {
-		const input = inputValueRef.current?.value.trim();
-		if (input) {
-			setSearchQuery(input);
-			setSelectedGenre(undefined);
-			setSelectedSorting("popularity.desc");
-			setselectedYear(undefined);
-		}
+	if (isLoading) {
+		return <p>Loading Content...</p>;
+	} else if (error) {
+		return <p>Run into Error: {error.message}</p>;
 	}
 
 	const lastCardIndex = currentPage * cardsPerPage;
 	const firstCardIndex = lastCardIndex - cardsPerPage;
-	const currentCards = mediaData?.slice(firstCardIndex, lastCardIndex);
-
-	// console.log(mediaData);
+	const currentCards = filteredData?.slice(firstCardIndex, lastCardIndex);
 
 	return (
 		<>
 			<h1 className="font-k2d text-white text-3xl font-bold p-4">
 				{mediaType === "movie" ? "Movies" : "TV Shows"}
 			</h1>
-			<FilterButtons
-				onGenreSelect={handleSelectGenres}
-				onYearSelect={handleSelectedYears}
-				onSortingSelect={handleSelectSorting}
-				onFilterEnter={handleDisplayMediaData}
-				inputValueRef={inputValueRef}
-			/>
+			<Filters setFilters={setFilters} />
 			<MediaCards mediaData={currentCards} />
 			<Pagination
-				totalCards={mediaData.length}
+				totalCards={filteredData.length}
 				cardsPerPage={cardsPerPage}
 				setCurrentPage={setCurrentPage}
 				currentPage={currentPage}
